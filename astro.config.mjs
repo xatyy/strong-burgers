560e0bb8dfa133f1e5b7cefbfe2fa136c0825502
chary-1 <56472718+chary-1@users.noreply.github.com>
2022-11-23 19:15:47 +0200

    robots 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,18 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 // https://astro.build/config
 //
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
+  site: "https://www.strongburgers.ro",
   integrations: [
     tailwind({
-      config: { path: "./tailwind.config.cjs" },
+      config: {
+        path: "./tailwind.config.cjs",
+      },
     }),
     react(),
+    sitemap(),
   ],
 });
